621bd2fa9deb5f184bc662ef6c69588808fca499
David Wogan <wogandavid@users.noreply.github.com>
2019-07-02 00:15:24 +0900

    Starting regression but...
    there are -INFs
    
    need to reshape?

--- industry/steel_cleaning.py
+++ industry/steel_cleaning.py
@@ -13,9 +13,9 @@ steel_history = steel.loc[:,'Indicator':'2016']
 steel_projections = steel.loc[:,'2017':'2050']
 
 # separate GDP, population, and steel production
-GDP = steel_history.loc[steel_history['Indicator'] == 'GDP'].drop(['Indicator'], axis=1).set_index('Economy')
-Pop = steel_history.loc[steel_history['Indicator'] == 'POP'].drop(['Indicator'], axis=1).set_index('Economy')
-ITM = steel_history.loc[steel_history['Indicator'] == 'ITM'].drop(['Indicator'], axis=1).set_index('Economy').replace(-1,0)
+GDP = steel_history.loc[steel_history['Indicator'] == 'GDP'].drop(['Indicator'], axis=1).set_index('Economy').transpose()
+Pop = steel_history.loc[steel_history['Indicator'] == 'POP'].drop(['Indicator'], axis=1).set_index('Economy').transpose()
+ITM = steel_history.loc[steel_history['Indicator'] == 'ITM'].drop(['Indicator'], axis=1).set_index('Economy').replace(-1,0).transpose()
 
 # divide to find per capita
 GDP_per_capita = GDP.div(Pop)
@@ -25,3 +25,11 @@ GDP_per_capita_ln = np.log(GDP_per_capita)
 Production_per_capita_ln = np.log(Production_per_capita)
 
 # regression
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+X = GDP_per_capita_ln
+y = Production_per_capita_ln
+
+reg = LinearRegression()
+reg.fit(X, y)
