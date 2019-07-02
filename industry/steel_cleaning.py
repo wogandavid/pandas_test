577616810355989cca857285dae997b896614f8a
David Wogan <wogandavid@users.noreply.github.com>
2019-07-02 13:38:27 +0900

    regression working
    but only one set of coefficients

--- industry/steel_cleaning.py
+++ industry/steel_cleaning.py
@@ -1,19 +1,34 @@
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 
 year = list(range(1980,2016))
 
 # read in CSV
 steel = pd.read_csv('IS_production2.csv', index_col=['Indicator','Economy'])
-steel = steel.stack().unstack(0)
+steel_history = steel.loc[:, '1980':'2016']
+steel_history = steel_history.stack().unstack(0)
 
-steel['GDP_per_capita_ln'] = np.log(steel['GDP'].div(steel['POP']))
-steel['Production_per_capita_ln'] = np.log(steel['ITM'].div(steel['POP']))
+steel_history['GDP_per_capita_ln'] = np.log(steel_history['GDP'].div(steel_history['POP']))
+steel_history['Production_per_capita_ln'] = np.log(steel_history['ITM'].div(steel_history['POP']))
+
+steel_history = steel_history.dropna()
+
+X = steel_history['GDP_per_capita_ln'].dropna().values.reshape(-1,1)
+y = steel_history['Production_per_capita_ln'].dropna().values.reshape(-1,1)
+
+reg = LinearRegression()
+reg.fit(X, y)
+
+print(reg.coef_)
+print(reg.intercept_)
+
 
 
 # separate historical data from projections
-steel_history = steel.loc[:,'Indicator':'2016']
-steel_projections = steel.loc[:,'2017':'2050']
+#steel_history = steel.loc[:,'Indicator':'2016']
+#steel_projections = steel.loc[:,'2017':'2050']
 
 # separate GDP, population, and steel production
 #GDP = steel_history.loc[steel_history['Indicator'] == 'GDP'].drop(['Indicator'], axis=1).set_index('Economy').transpose()
@@ -25,25 +40,25 @@ steel_projections = steel.loc[:,'2017':'2050']
 #ITM = steel_history.loc[steel_history['Indicator'] == 'ITM'].drop(['Indicator'], axis=1).set_index('Economy').stack()
 
 # divide to find per capita
-GDP_per_capita = GDP.div(Pop)
-Production_per_capita = ITM.div(Pop)
+#GDP_per_capita = GDP.div(Pop)
+#Production_per_capita = ITM.div(Pop)
 
-data = pd.merge(GDP_per_capita, Production_per_capita, how='left')
+#data = pd.merge(GDP_per_capita, Production_per_capita, how='left')
 
-GDP_per_capita_ln = np.log(GDP_per_capita).dropna()
-Production_per_capita_ln = np.log(Production_per_capita).dropna()
+#GDP_per_capita_ln = np.log(GDP_per_capita).dropna()
+#Production_per_capita_ln = np.log(Production_per_capita).dropna()
 
-data 
+#data 
 
 # regression
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
+#from sklearn.linear_model import LinearRegression
+#from sklearn.metrics import mean_squared_error, r2_score
 
-X = GDP_per_capita_ln.values.reshape(-1,1)
-y = Production_per_capita_ln.values.reshape(-1,1)
+#X = GDP_per_capita_ln.values.reshape(-1,1)
+#y = Production_per_capita_ln.values.reshape(-1,1)
 
-reg = LinearRegression()
-reg.fit(X, y)
+#reg = LinearRegression()
+#reg.fit(X, y)
 
-print(reg.coef_)
-print(reg.intercept_)
+#print(reg.coef_)
+#print(reg.intercept_)
