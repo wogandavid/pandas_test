3ba0618eb7bddfcde75ddfd2c385cb467f9fa4a5
David Wogan <wogandavid@users.noreply.github.com>
2019-07-07 13:32:08 +0900

    Added Linear Regression for one economy

---
+++ tidy/LinearRegression.py
@@ -0,0 +1,33 @@
+# Run a linear regression using historical steel production, GDP, and population
+# Project production using GDP and population projections
+
+import pandas as pd
+import numpy as np
+import matplotlib as plt 
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+# read in prepared data from CSV
+SteelDataHistoricalPrepared = pd.read_csv('SteelDataHistoricalPrepared.csv')
+
+# perform regression over one economy as a test
+SteelDataHistoricalPreparedAUS = SteelDataHistoricalPrepared[SteelDataHistoricalPrepared.Economy == '01_AUS']
+
+# select features and reshape 
+X = SteelDataHistoricalPreparedAUS['GDPperCapita'].values.reshape(-1,1)
+Y = SteelDataHistoricalPreparedAUS['SteelProductionperCapita'].values.reshape(-1,1)
+
+# define linear regression object to store the model
+reg = LinearRegression()
+reg.fit(X, Y)
+
+# print results
+print(reg.coef_)
+print(reg.intercept_)
+
+# make a prediction using the model
+prediction = reg.predict(X)
+
+# plot regression
+plt.scatter(X,Y)
+plt.plot(X,prediction)
