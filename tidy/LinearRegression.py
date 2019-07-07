ef660a58ad2802ddeec5e35f613cf31a8b16c77f
David Wogan <wogandavid@users.noreply.github.com>
2019-07-07 13:46:39 +0900

    Add pyplot import

--- tidy/LinearRegression.py
+++ tidy/LinearRegression.py
@@ -3,7 +3,7 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib as plt 
+import matplotlib.pyplot as plt 
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
