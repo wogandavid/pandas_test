b4e7746dfa4358a1e2a95262fa8f0a7a48d0d103
David Wogan <wogandavid@users.noreply.github.com>
2019-07-10 20:05:29 -0400

    Looping successful in Loopmodels

--- industry/loopmodels.py
+++ industry/loopmodels.py
@@ -21,16 +21,47 @@ models = {economy: LinearRegression() for economy in economies}
 
 # set Economy as index and set target vector
 df = (SteelDataHistoricalPrepared.set_index('Economy')
-                                 .drop(['Year','GDP','SteelProduction','Population'], axis=1))
+                                 .drop(['GDP','SteelProduction','Population'], axis=1))
 
 # loop over economy-model pairs to fit regression
 for economy, model in models.items():
         model.fit(df.loc[economy, :].drop('SteelProductionperCapita', axis=1),df.loc[economy, 'SteelProductionperCapita'])
 #        print(model.coef_)
 
-# store output
-results = pd.DataFrame(columns=['prediction'], index=economies)
-
-# loop over economy-model pairs
+# loop over economy-model pairs to make prediction and write prediction to csv, one for each economy
+#index2 = SteelDataHistoricalPrepared[['Economy','Year']]
+df2 = SteelDataHistoricalPrepared[['Economy','Year']]
+filelist = []
+#series_list = []
 for economy, model in models.items():
-        results.loc[economy,'prediction'] = model.predict(df.loc[economy, :].drop('SteelProductionperCapita', axis=1))
+        prediction = model.predict(df.loc[economy, :].drop('SteelProductionperCapita', axis=1))
+#        series_list.append(pd.Series(data=prediction, index = index2))
+        results = df2[df2.Economy == economy]
+        results['prediction'] = prediction
+#        results = pd.Series(data=prediction, index=df.loc[economy,'Year'])
+        newfilename = '%sPrediction.csv' %economy
+        results.to_csv('results\%s' %newfilename, header=True)
+#        results_df.to_csv('results\%s' %newfilename, header=True)
+        filelist.append(newfilename)
+
+# read in all csv and combine to one df
+df_list =[]
+for economy in economies:
+        newfilename = '%sPrediction.csv' %economy
+        df_list.append(pd.read_csv('results\%s' %newfilename))
+dfResults = pd.concat(df_list).drop('Unnamed: 0', axis=1)
+dfResults['GDPperCapita'] = SteelDataHistoricalPrepared['GDPperCapita']
+
+# plot using seaborn
+import seaborn as sns
+g = sns.lmplot(x="GDPperCapita", 
+               y="prediction", 
+               col="Economy", 
+               data=dfResults, 
+               col_wrap=3,
+               height= 3, 
+               aspect=1, 
+               sharex= False,
+               sharey= False,
+               fit_reg = False)
+
